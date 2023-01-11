d812eba0d072c46b16e1339a87390ece18aa4fdf
wangyingjie <wangyingjie@piesat.cn>
2023-01-11 11:00:18 +0800

    feat: redis缓存

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { CacheModule, Module, CacheStore } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { getConfig } from './utils';
 
+const redisConfig = getConfig().REDIS_CONFIG;
 @Module({
   imports: [
-    CacheModule.register({
+    // 全局的cacheModule
+    CacheModule.registerAsync({
       isGlobal: true,
+      useFactory: async () => {
+        const store = await redisStore({
+          socket: {
+            host: redisConfig.host,
+            port: +redisConfig.port,
+          },
+          password: redisConfig.auth,
+          ttl: 60 * 60 * 64 * 7,
+        });
+        return {
+          store: {
+            create: () => store as unknown as CacheStore,
+          },
+        };
+      },
     }),
     // 全局解析ymal配置文件
     ConfigModule.forRoot({
